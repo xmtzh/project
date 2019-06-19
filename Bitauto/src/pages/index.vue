917242127ca4b98b0fdda33c8e4f2ca4b3bc8beb
xmtzh <1318226678@qq.com>
2019-06-20 00:28:30 +0800

    切图

--- Bitauto/src/pages/index.vue
+++ Bitauto/src/pages/index.vue
@@ -37,6 +37,29 @@
         <img src="../images/searchBtn.png" alt="">
       </div>
     </div>
+    <!-- 条件搜车tabbar -->
+    <div class="search-car-tabbar">
+      条件搜车
+    </div>
+    <!-- 轮播图 -->
+    <div class="wrapper">
+        <swiper :options="swiperOption" >
+            <swiper-slide v-for='item of swiperList' :key="item.id">
+                <img :src="item.imgUrl" class="swiper-img">
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+    <!-- 四方格 -->
+    <div class="tuwen">
+      <router-link class="tuwen-one" to="#" v-for="(item,index) in tuwenInfo" :key="index">
+        <img :src="item.img" alt="">
+        <div class="tuwen-text">
+          <span class="tuwen-text-title">{{item.title}}</span>
+          <span class="tuwen-text-desc">{{item.desc}}</span>
+        </div>
+      </router-link>
+    </div>
 
   </div>
 </template>
@@ -63,8 +86,90 @@ export default {
         '昂科威',
         '马自达'
       ],
-      searchName:''
+      searchName:'',
+      carTabbar:[
+        {
+          name:'按品牌'
+        },
+        {
+          name:'按条件'
+        },
+        {
+          name:'热门车'
+        },
+        {
+          name:'新能源'
+        },
+        {
+          name:'二手车'
+        }
+      ],
+      // 轮播设置
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true,
+        autoplay: 2000,
+        speed:1000
+      },
+      // 轮播图片
+      swiperList:[
+        {
+          id:"0001",
+          imgUrl:"https://dd.yiche.com/inner-dsp-public/c4df1169251a447b3d6bfea7c9634812.jpg"
+        },
+        {
+          id:"0002",
+          imgUrl:"http://img1.bitautoimg.com/bitauto/2019/06/19/cbfd947d-6732-4a98-822a-32552463c249.jpg"
+        },
+        {
+          id:"0003",
+          imgUrl:"http://img1.bitautoimg.com/bitauto/2019/06/19/14e80b31-fef8-4efc-8652-48ff7c978073.jpg"
+        },
+        {
+          id:"0004",
+          imgUrl:"http://image.bitautoimg.com/bitauto/2019/06/17/e1a61ff0-4cfd-4a06-9a24-da5a6f0393ee.jpg"
+        },
+        {
+          id:"0005",
+          imgUrl:"https://dd.yiche.com/inner-dsp-public/cc88b1d209e18e321bc437aec5e3a77d.jpg"
+        },
+        {
+          id:"0006",
+          imgUrl:"https://dd.yiche.com/inner-dsp-public/48e53e2acfc1be2a778b35664590869d.jpg"
+        },
+        {
+          id:"0007",
+          imgUrl:"https://dd.yiche.com/inner-dsp-public/8188ce238868ad65289b455fb34b9ea4.jpg"
+        },
+      ],
+      // 四方个格图文数据1
+      tuwenInfo:[
+        {
+          img:'http://img2.bitautoimg.com/bitauto/2018/02/06/92e801c6-e564-4f97-8446-9d6f2bac760a.jpg',
+          title:'一成首付',
+          desc:'新车开走吧'
+        },
+        {
+          img:'http://img2.bitautoimg.com/bitauto/2017/06/29/96a5da43-e5df-4969-9052-1b10693dc542.jpg',
+          title:'帮你买车',
+          desc:'低价买车不吃亏'
+        },
+        {
+          img:'http://image.bitautoimg.com/bitauto/2018/02/06/994612f9-a8d6-45bb-85c4-89ba6e28f19a.jpg',
+          title:'新能源',
+          desc:'挑款合适电动车'
+        },
+        {
+          img:'http://img2.baa.bitautotech.com/usergroup/editor_pic/2018/5/25/8fa7777edbf44dd98144df072e9a7fad.jpg',
+          title:'买车神器',
+          desc:'真实价格发票为证'
+        }
+      ],
+
     }
+  },
+  components:{
+
   }
 }
 </script>
@@ -159,10 +264,54 @@ export default {
       position absolute
       left 50%
       transform translate(-50%)
-
-
-
-
+.search-car-tabbar
+  padding 0 2rem
+  height 10rem
+  width 100%
+  box-sizing border-box
+  background-color #3677E0
+.wrapper >>> .swiper-pagination-bullet-active
+  background red
+  width 0.5rem
+  height 0.5rem
+.wrapper >>> .swiper-pagination-bullet
+  width 0.5rem
+  height 0.5rem
+.wrapper
+    overflow: hidden
+    width: 100%
+    background: #ccc
+    .swiper-img
+      width: 100%
+.tuwen
+  padding 0 2rem
+  width 100%
+  height 10rem
+  box-sizing border-box
+  .tuwen-one
+    position relative
+    width 50%
+    display block
+    float left
+    border-bottom 0.1rem solid #f5f7fb
+    box-sizing border-box
+    &:nth-child(odd)
+      border-right 0.1rem solid #f5f7fb
+    img
+      width 100%
+      z-index 1
+      display block
+    .tuwen-text
+      position absolute
+      top 2rem
+      left 1rem
+      color #999
+      font-size 1.2rem
+      .tuwen-text-title
+        display block
+        color #000
+        font-weight bolder
+        font-size 1.6rem
 
 
 
